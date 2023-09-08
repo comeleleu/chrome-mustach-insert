a0d23461e192d4276ef18d76efcb9511acd6b1c2
Pascal Breton-Faguy <pascal.breton-faguy@libeo.com>
2023-09-08 13:00:37 -0400

    [FIX] remove integration

--- integration/src/js/app.js
+++
@@ -1,8 +0,0 @@
-import 'what-input';
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('ready!');
-});
-
-// Module import exemple with Code Splitting
-// const AModule = () => import(/* webpackChunkName: "a-module"*/ './modules/AModule');
