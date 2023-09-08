2afd344772ff2583e51e819c02c289af824b24c3
Vincent Cossette <vincent@rumandcode.io>
2023-09-08 01:07:14 -0400

    Merge support for multiple mustachification.  None shall be left wanting for a lips coat in winter

--- content.js
+++ content.js
@@ -1,21 +1,11 @@
-
-String.prototype.hashCode = function() {
-  var hash = 0,
-    i, chr;
-  if (this.length === 0) return hash;
-  for (i = 0; i < this.length; i++) {
-    chr = this.charCodeAt(i);
-    hash = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-}
-
 /*
 Get image, detect and recognize faces, then update src image
 */
 function processImage(node) {
+  if (node.getAttribute("data-stached") == "true") { return }
+
   node.crossOrigin = "anonymous";
+
   if (node.complete) {
     f = async () => add_mustache(node)
     f();
@@ -26,26 +16,24 @@ function processImage(node) {
   }
 }
 
-function add_mustache(node) {
+async function add_mustache(node) {
+  if (node.getAttribute("data-stached") == "true") { return }
   const canvas = faceapi.createCanvasFromMedia(node);
-  document.body.append(canvas);
+  // document.body.append(canvas);
   const displaySize = { width: node.width, height: node.height };
   faceapi.matchDimensions(canvas, displaySize);
 
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(node, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    if (resizedDetections[0] == undefined) { return }
+  const detections = await faceapi.detectAllFaces(node, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
+  const resizedDetections = faceapi.resizeResults(detections, displaySize);
+  if (resizedDetections[0] == undefined) {
+    node.setAttribute("data-stached", true);
+    return
+  }
 
-    draw(node, resizedDetections[0].landmarks)
-  }, 1000);
+  draw(node, resizedDetections)
 }
 
-function draw(img, landmarks) {
-  const nose = landmarks._positions[33];
-  const mustacheWidth = (landmarks._positions[54]._x - landmarks._positions[48]._x) * 2;
-  const mustacheHeight = mustacheWidth / 3;
-
+async function draw(img, resizedDetections) {
   const stach = chrome.runtime.getURL("images/mustaches/mustache_1.png")
 
   var image = new Image();
@@ -58,8 +46,16 @@ function draw(img, landmarks) {
     canvas.width = img.width;
     canvas.height = img.height;
     context.drawImage(img, 0, 0);
-    context.drawImage(image, nose._x - mustacheWidth / 2, nose._y - mustacheHeight / 4, mustacheWidth, mustacheHeight);
+
+    resizedDetections.forEach((face) => {
+      const nose = face.landmarks._positions[33];
+      const mustacheWidth = (face.landmarks._positions[54]._x - face.landmarks._positions[48]._x) * 2;
+      const mustacheHeight = mustacheWidth / 3;
+      context.drawImage(image, nose._x - mustacheWidth / 2, nose._y - mustacheHeight / 4, mustacheWidth, mustacheHeight);
+    })
+
     img.src = canvas.toDataURL()
+    img.setAttribute("data-stached", true);
   }
 }
 
@@ -75,9 +71,10 @@ async function loadModels() {
 document.addEventListener("DOMContentLoaded", async function (event) {
   await loadModels();
 
-  Array.from(document.images).forEach((img) => {
-    processImage(img);
-  });
-
+  setInterval(async () => {
+    Array.from(document.images).forEach((img) => {
+      processImage(img);
+    });
+  }, 1000);
   // observeMutations();
 });
