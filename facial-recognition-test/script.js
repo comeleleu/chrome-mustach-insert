06f393509c3c37f64063a8c14cb7120d6ce1935c
Vincent Cossette <vincent@rumandcode.io>
2023-09-07 20:54:03 -0400

    Display the 'stache of power in test app

--- facial-recognition-test/script.js
+++ facial-recognition-test/script.js
@@ -12,7 +12,7 @@ function startVideo() {
   navigator.getUserMedia(
     { video: {} },
     (stream) => (video.srcObject = stream),
-    (err) => console.error(err)
+    (err) => console.log("oops")
   );
 }
 if (luc) {
@@ -25,20 +25,30 @@ function checkFace() {
   const canvas = faceapi.createCanvasFromMedia(luc);
   document.body.append(canvas);
   const displaySize = { width: luc.width, height: luc.height };
-  faceapi.matchDimensions(canvas, displaySize);
+  faceapi.matchDimensions(canvas, displaySize); 
+  
   setInterval(async () => {
     const detections = await faceapi
       .detectAllFaces(luc, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceExpressions();
-    console.log(detections);
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    console.log(resizedDetections);
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    faceapi.draw.drawDetections(canvas, resizedDetections);
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-  }, 100);
+
+    const nose = detections[0].landmarks._positions[33];
+
+    var image = new Image();
+    image.src = "/facial-recognition-test/images/new_mustache.png";
+    image.onload = function(ev) {
+       var ctx = canvas.getContext('2d');
+       ctx.drawImage(image, (nose._x/2)-30, nose._y/2, 60, 22);
+    }
+
+    // const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    // console.log(resizedDetections);
+    // canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    // faceapi.draw.drawDetections(canvas, resizedDetections);
+    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 1000);
 }
 
 // video.addEventListener("play", () => {
